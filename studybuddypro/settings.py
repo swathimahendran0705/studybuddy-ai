c70f39e0ac98629203dab9e905a3d553d00a0ff0
Swathi <swathimahendran0705@gmail.com>
2026-02-18 16:55:08 +0530

    Brevo email setup

--- studybuddypro/settings.py
+++ studybuddypro/settings.py
@@ -137,13 +137,16 @@ if MONGO_URI:
 
 # ================= EMAIL =================
 # ================= EMAIL =================
+# ================= EMAIL =================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")   # Brevo email
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # Brevo API key
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+
 
