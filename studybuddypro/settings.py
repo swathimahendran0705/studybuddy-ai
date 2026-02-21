37bef604c1f3c51a13b04311ff13e8d9ccdfad70
Swathi <swathimahendran0705@gmail.com>
2026-02-21 10:36:19 +0530

    Fixed email sender using Brevo SMTP

--- studybuddypro/settings.py
+++ studybuddypro/settings.py
@@ -137,16 +137,13 @@ if MONGO_URI:
 
 # ================= EMAIL =================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
 EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 30
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-
-
 
